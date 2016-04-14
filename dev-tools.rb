38fda443b1116e8b4661265a080b0e21cb59b48b
Jean-Marc Lagace <jean-marc@m2i3.com>
2016-04-14 14:18:08 -0400

    release the dev tools tap

---
+++ dev-tools.rb
@@ -0,0 +1,33 @@
+# Documentation: https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class DevTools < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/jmlagace/dev-tools/archive/1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "ad085c5ca288f7d1e3df2f7f470ed548e1383f026505ec4f13677c71362f2bd9"
+
+  # depends_on "cmake" => :build
+  # depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    bin.install "php-check"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test dev-tools`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "true"
+  end
+end
